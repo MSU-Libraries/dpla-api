58de19a1daf64d5ea9c3502742fec4d197b42047
Devin Higgins <higgi135.mail.lib.msu.edu>
2015-02-27 17:27:21 -0500

    Working on JSON->XML

--- dpla_api.py
+++ dpla_api.py
@@ -1,17 +1,46 @@
 from dpla.api import DPLA
 from pprint import pprint
+from lxml import etree
 
 
 class DplaApi():
     def __init__(self, dpla_key="07c4dfca29876f8b591732735913abb4"):
         self.dpla_key=dpla_key
         self.dpla = DPLA(dpla_key)
+        self.result = None
 
     def search(self, q_value, page_size=100):
         self.result = self.dpla.search(q=q_value, page_size=page_size)
         print "===First Result==="
         print "---Access [instance].result.items object to see all---"
         pprint(self.result.items[0])
+
+    def return_marcxml(self):
+        if self.result is None:
+            print "Error -- No data available -- Run search first"
+            return
+
+        for item in self.result.items:
+            json_record = item["originalRecord"]
+            self.build_marcxml_record(json_record)
+
+    def build_marcxml_record(self, json_record):
+        root = etree.Element("record", xmlns="http://www.loc.gov/MARC21/slim")
+        leader = etree.SubElement(root, "leader")
+        leader.text = json_record["leader"]
+        for c in json_record["controlfield"]:
+            controlfield = etree.SubElement(root, "controlfield", tag=c["tag"])
+            controlfield.text = c["#text"]
+
+            for row in q_datafields:
+    datafield = etree.SubElement(root,"datafield",tag=row.tag,ind1=row.ind1,ind2=row.ind2)
+    q_subfields = Subfields.objects.using("metagds").filter(datafield=row.datafield_id)
+    for sub_row in q_subfields:
+        subfield = etree.SubElement(datafield,"subfield",code=sub_row.code)
+        subfield.text = sub_row.content
+
+
+
 
     def return_html(self, filepath="dpla.html"):
         with open(filepath, "w") as f:
