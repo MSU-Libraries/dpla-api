b9f515bc0c6c27178bfb7ad436578a57286e34bb
Devin Higgins <higgi135.mail.lib.msu.edu>
2015-08-12 11:54:34 -0400

    Update method to perform sequence of searches and store results

--- dpla_api.py
+++ dpla_api.py
@@ -1,21 +1,84 @@
+from __future__ import division
 from dpla.api import DPLA
 from pprint import pprint
 from lxml import etree
 import ConfigParser
+from metadpla import DplaMetadata
+import codecs
+import time
+import json
+import math
 
 
 class DplaApi():
     """Interact with DPLA API using pre-acquired key."""
     def __init__(self):
-        self.dpla_key=__load_dpla_key()
-        self.dpla = DPLA(dpla_key)
+        self.dpla_key= self.__load_dpla_key()
+        self.dpla = DPLA(self.dpla_key)
         self.result = None
+        self.metadata_records = []
 
     def search(self, q_value, page_size=100):
+        """
+        Run basic search query across DPLA.
+        q_value (str) -- value to search
+        page_size (int) -- max number of results to request from DPLA. (DPLA-imposed limit is 500)
+        """
+        self.query = q_value
+        print "Query: {0}".format(self.query)
         self.result = self.dpla.search(q=q_value, page_size=page_size)
-        print "First Result"
-        print "Access [instance].result.items object to see all"
-        pprint(self.result.items[0])
+        time.sleep(.5)
+        self.all_returned_items = self.result.items
+        if self.result.count > self.result.limit:
+            pages = int(math.ceil(self.result.count / self.result.limit))
+            for page in range(2, pages + 1):
+                print "----Accessing results page {0}".format(page)
+                self.result = self.dpla.search(q=q_value, page_size=page_size, page=page)
+                self.all_returned_items += self.result.items
+                time.sleep(.5)
+
+        
+
+
+    def build_arc_rdf_dataset(self, check_match=True):
+        """Iterate over search results and pull necessary elements to create ARC RDF, storing them in a list of python dictionaries."""
+
+        if check_match:
+            self.__load_match_data()
+
+        print "Query '{0}' returned {1} records (Check: {2} records transferred)".format(self.query, self.result.count, len(self.all_returned_items))
+
+        for item in self.all_returned_items:
+
+            if check_match:
+                if item["@id"] in self.existing_records:
+                    pass
+                else:
+                    self.__process_metadata(item)
+                    self.existing_records.append(item["@id"])
+
+            else:
+                self.__process_metadata(item)
+
+        
+
+    def create_tsv(self, output_path="data/radicalism-all-dpla.tsv"):
+        """Build CSV file for ARC pre-RDF dataset."""
+        tsv_text = ""
+        headings = "\t".join(self.metadata_records[0].keys())
+        tsv_text += headings + "\n"
+        for record in self.metadata_records:
+            for key, value in record.items():
+                if isinstance(value, list):
+                    tsv_text += " | ".join(value) 
+                else:
+                    tsv_text += value
+                tsv_text += "\t"
+            tsv_text += "\n"
+
+        with codecs.open(output_path, "w", "UTF-8") as output_file:
+            output_file.write(tsv_text)
+
 
     def return_marcxml(self):
         if self.result is None:
@@ -66,8 +129,45 @@ class DplaApi():
 
     def __load_dpla_key(self):
         """Load DPLA API Key from config file."""
-        config = configParser.RawConfigParser()
+        config = ConfigParser.RawConfigParser()
         config.read("default.cfg")
         return config.get("dpla_api", "api_key")
+
+    def __load_match_data(self):
+        """Prepare data on previous search results."""
+        match_file, reset_match_file = self.__load_match_settings()
+        if reset_match_file == "true":
+            self.existing_records = []
+        else:
+            self.existing_records = json.load(open(match_file, "r"))
+
+    def __load_match_settings(self):
+        """Load file containing list of all previously processed items."""
+        config = ConfigParser.RawConfigParser()
+        config.read("default.cfg")
+        return config.get("check_match", "match_file"), config.get("check_match", "reset_match_file")
+
+    def __process_metadata(self, item):
+        """Process metadata from JSON DPLA record, pulling descriptive elements from the 'sourceResource' fields.
+
+        Positional arguments:
+        item (dict) -- Python dictionary from JSON results of DPLA search.
+        """
+        d_metadata = DplaMetadata(item["sourceResource"])
+        d_metadata.compile()
+        d_metadata.record["thumbnail"] = item.get("object", "")
+        d_metadata.record["seeAlso"] = item.get("isShownAt", "")
+        if "provider" in item:
+            d_metadata.record["archive"] = item["provider"]["name"]
+        elif "dataProvider" in item:
+            d_metadata.record["archive"] = item["dataProvider"]
+        else:
+            d_metadata.record["archive"] = ""
+        d_metadata.record["discipline"] = ""
+        d_metadata.record["genre"] = ""
+        d_metadata.record["federation"] = "SiRO"
+        d_metadata.record["original_query"] = self.query
+        self.metadata_records.append(d_metadata.record)
+
 
 
---
+++ metadpla.py
@@ -0,0 +1,70 @@
+
+
+class DplaMetadata():
+
+    def __init__(self, dpla_metadata):
+        """
+        Initialize object containing just the DPLA-level metadata (not from original record).
+
+        Positional arguments:
+        dpla_metadata (dict) -- object of "sourceResource"-level metadata. See dpla_sample_metadata.json for details.
+        """
+        self.metadata = dpla_metadata
+
+    def compile(self):
+        """Pull relevant metadata for ARC record, and record fields not found."""
+        record = {
+
+                        "date":"",
+                        "title":"",
+                        "subjects":[],
+                        "type":[],
+                        "creator":[],
+                        "language":[],
+                    }
+        
+
+        if "date" in self.metadata:
+            if isinstance(self.metadata["date"], list):
+                
+                if "displayDate" in self.metadata["date"][0]:
+                    record["date"] = self.metadata["date"][0]["displayDate"]
+                elif "begin" in self.metadata["date"][0]:
+                    record["date"] = self.metadata["date"][0]["begin"]
+
+                elif "end" in self.metadata["date"][0]:
+                    record["date"] = self.metadata["date"][0]["end"]
+
+            elif isinstance(self.metadata["date"]["displayDate"], list):
+                record["date"] = self.metadata["date"]["displayDate"][0]
+
+            else:
+                record["date"] = self.metadata["date"]["displayDate"]
+
+        if "title" in self.metadata:
+            if isinstance(self.metadata["title"], list):
+                record["title"] = self.metadata["title"][0]
+
+            else:
+                record["title"] = self.metadata["title"]
+
+        if "subject" in self.metadata:
+            record["subjects"] = [subject["name"] for subject in self.metadata["subject"]]
+
+        if "specType" in self.metadata:
+            record["type"] = self.metadata["specType"]
+
+        if "creator" in self.metadata:
+            record["creator"] = self.metadata["creator"]
+
+        if "language" in self.metadata:
+
+            record["language"] = [lang["name"] for lang in self.metadata["language"]]
+
+
+        self.record = record
+
+
+
+
+
