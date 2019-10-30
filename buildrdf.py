496b77f1b86d4c587d335eee9f0ea5a41a6b72a2
Devin Higgins <higgi135@msu.edu>
2019-10-30 17:54:03 -0400

    Update to python3 underway

--- buildrdf.py
+++ buildrdf.py
@@ -8,12 +8,12 @@ import codecs
 class BuildRdf():
     """Class to build RDF according to ARC standard."""
 
-    def __init__(self):
+    def __init__(self, archive="dpla"):
         """Load default values."""
         self.default_values = {
             "discipline": ["History"],
             "role": ["CRE"],
-            "archive": ["dpla"],
+            "archive": archive,
             "genre": ["Unspecified"],
             "freeculture": ["TRUE"]
         }
@@ -52,7 +52,7 @@ class BuildRdf():
                 output_file.write(etree.tostring(self.rdf_root, encoding="unicode", pretty_print=True))
 
         else:
-            print "Invalid path -- File Doesn't Exist: {0}".format(self.tsv_path)
+            print("Invalid path -- File Doesn't Exist: {0}".format(self.tsv_path))
 
     def __read_tsv(self):
         """Open and read file."""
@@ -61,7 +61,7 @@ class BuildRdf():
             if not self.lines_to_process:
                 self.lines_to_process = len(tsv_file.readlines())
 
-            print "Processing {0} records...".format(self.lines_to_process)
+            print("Processing {0} records...".format(self.lines_to_process))
 
             # The previous readlines() call moved the pointer to the end of the file, making iteration
             # impossible. This line resets the pointer.
@@ -86,7 +86,7 @@ class BuildRdf():
         self.line_reference = {}
         self.line_reference.update(zip(self.headings, self.line_data))
         if self.lines_processed % 500 == 0:
-            print "Processed {0} records".format(self.lines_processed)
+            print("Processed {0} records".format(self.lines_processed))
         """
         print "{0} : {1}".format(
             self.line_reference["id"].encode("ascii", errors="ignore"),
@@ -99,13 +99,13 @@ class BuildRdf():
         """Start RDF creation."""
 
         self.ns_map = {
-            "rdf":"http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-            "role":"http://www.loc.gov/loc.terms/relators/",
-            "rdfs":"http://www.w3.org/2000/01/rdf-schema#",
-            "collex":"http://www.collex.org/schema#",
-            "dcterms":"http://purl.org/dc/terms/",
-            "dc":"http://purl.org/dc/elements/1.1/",
-            "sro":"http://www.lib.msu.edu/sro/schema#",
+            "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+            "role": "http://www.loc.gov/loc.terms/relators/",
+            "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+            "collex": "http://www.collex.org/schema#",
+            "dcterms": "http://purl.org/dc/terms/",
+            "dc": "http://purl.org/dc/elements/1.1/",
+            "sro": "http://www.lib.msu.edu/sro/schema#",
         }
 
         # self.rdf_root = etree.Element("{{{0}}}RDF".format(self.ns_map["rdf"]), nsmap=self.ns_map)
@@ -115,43 +115,53 @@ class BuildRdf():
         """Process data into RDF."""
         # The triple-{} includes a literal "{}" and a {} that operates the format string.
         # That is, one escapes braces in a format string by doubling them.
-        self.siro_wrapper = etree.Element("{{{0}}}dpla".format(self.ns_map["sro"]))
+        self.siro_wrapper = etree.Element("{{{0}}}{1}".format(self.ns_map["sro"], self.default_values["archive"]))
         siro_wrapper_attribute = "{{{0}}}about".format(self.ns_map["rdf"])
         self.siro_wrapper.attrib[siro_wrapper_attribute] = self.line_reference["id"]
-        federation = self.__add_subelement(self.siro_wrapper, "federation", "collex", field_value="SiRO")
+        self.__add_subelement(self.siro_wrapper, "federation", "collex", field_value="SiRO")
 
         for value in self.__get_values("archive"):
-            archive = self.__add_subelement(self.siro_wrapper, "archive", "collex", field_value=value)
-        
-        title = self.__add_subelement(self.siro_wrapper, "title", "dc", field_value=self.line_reference["title"])
+            self.__add_subelement(self.siro_wrapper, "archive", "collex", field_value=value)
+
+        self.__add_subelement(self.siro_wrapper, "title", "dc", field_value=self.line_reference["title"])
         self.__add_creators()
-        source = self.__add_subelement(self.siro_wrapper, "source", "dc", field_value=self.line_reference["source"])
-        
+        self.__add_subelement(self.siro_wrapper, "source", "dc", field_value=self.line_reference["source"])
+        self.__add_subelement(
+            self.siro_wrapper,
+            "description", "dc",
+            field_value="\n".join([d.strip(":").strip() for d in self.line_reference["description"].split("|")])
+        )
+
         for subject in self.line_reference["subjects"].split("|"):
-            subject = self.__add_subelement(self.siro_wrapper, "subject", "dc", field_value=subject.strip())
+            self.__add_subelement(self.siro_wrapper, "subject", "dc", field_value=subject.strip())
 
         for value in self.__get_values("discipline"):
-            archive = self.__add_subelement(self.siro_wrapper, "discipline", "collex", field_value=value)
-        
+            self.__add_subelement(self.siro_wrapper, "discipline", "collex", field_value=value)
+
         self.__add_genres()
         self.__add_freecultures()
         self.__add_type()
         self.__add_date()
 
-        fulltext = self.__add_subelement(self.siro_wrapper, "fulltext", "collex", field_value="TRUE")
+        self.__add_subelement(self.siro_wrapper, "fulltext", "collex", field_value="TRUE")
         if self.line_reference["language"].strip():
-            language = self.__add_subelement(self.siro_wrapper, "language", "dc", field_value=self.line_reference["language"])
-        
-        if self.line_reference["seeAlso"].strip():
+            self.__add_subelement(self.siro_wrapper, "language", "dc", field_value=self.line_reference["language"])
+
+        if self.line_reference.get("seeAlso", "").strip():
             object_link = self.line_reference["seeAlso"].strip()
+        elif self.line_reference["see_also"].strip():
+            object_link = self.line_reference["see_also"].strip()
         else:
             object_id = os.path.basename(self.line_reference["id"])
             object_link = "http://dp.la/item/{0}".format(object_id)
 
-        see_also = self.__add_subelement(self.siro_wrapper, "seeAlso", "rdfs", attributes={"{{{0}}}resource".format(self.ns_map["rdf"]): object_link})
+        self.__add_subelement(
+            self.siro_wrapper, "seeAlso", "rdfs",
+            attributes={"{{{0}}}resource".format(self.ns_map["rdf"]): object_link}
+        )
 
         if self.line_reference["thumbnail"].strip():
-            thumbnail = self.__add_subelement(self.siro_wrapper, "thumbnail", "collex", attributes={"{{{0}}}resource".format(self.ns_map["rdf"]): self.line_reference["thumbnail"]})
+            self.__add_subelement(self.siro_wrapper, "thumbnail", "collex", attributes={"{{{0}}}resource".format(self.ns_map["rdf"]): self.line_reference["thumbnail"]})
 
         return self.siro_wrapper
         """
@@ -174,12 +184,20 @@ class BuildRdf():
 
     def __add_creators(self):
         """Bring together roles and creators."""
-        if self.line_reference["creator"].strip():
-            self.creators = self.line_reference["creator"].split("|")
-        else:
-            self.creators = ["Unknown"]
-        self.role_type = self.__get_role()
-        creator_roles = zip(self.role_type, self.creators)
+        if "creator" in self.line_reference:
+
+            if self.line_reference["creator"].strip():
+                self.creators = self.line_reference["creator"].split("|")
+            else:
+                self.creators = ["Unknown"]
+            self.role_type = self.__get_role()
+            creator_roles = zip(self.role_type, self.creators)
+        elif "creators" in self.line_reference:
+            creator_roles = []
+            for creator_entry in self.line_reference["creators"].split("|"):
+                creator, role = creator_entry.split(":")
+                creator_roles.append((role.strip(), creator.strip()))
+
         for creator in creator_roles:
             self.__add_subelement(self.siro_wrapper, creator[0], "role", field_value=creator[1].strip())
 
@@ -188,7 +206,7 @@ class BuildRdf():
         if self.line_reference["genre"] == "none":
             self.genres = []
         else:
-            self.genres = [self.line_reference["genre"].title()]
+            self.genres = [g.title() for g in self.line_reference["genre"].split("|")]
 
         self.letter_pattern = r'\[.*[Ll]etter.*\]'
         if re.search(self.letter_pattern, self.line_reference["title"]):
@@ -236,6 +254,8 @@ class BuildRdf():
     def __add_date(self):
         """Heuristic for date handling."""
         date = self.line_reference["date"]
+        if "|" in date:
+            date = date.split("|")[0]
         dcdate = None
 
         matches = re.findall(r'[0-9]{4}', date)
@@ -245,7 +265,10 @@ class BuildRdf():
 
         elif matches:
             date_label = date
-            date_value = ",".join([min(matches), max(matches)])
+            if min(matches) != max(matches):
+                date_value = ",".join([min(matches), max(matches)])
+            else:
+                date_value = min(matches)  # Arbitrarily choose one over the other if they are the same.
 
         elif date.strip():
             date_label = date
@@ -272,7 +295,6 @@ class BuildRdf():
     def __get_role(self):
         """Check for role value in tsv data, use default if blank."""
         if not self.line_reference["role"].strip():
-
             return ["CRE"] * len(self.creators)
         else:
             return [role.strip() for role in self.line_reference["role"].split("|")]
